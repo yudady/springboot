7603fb00e928f5b1271202c3dbcac8f4277b56e7
yu_dady@yahoo.com.tw <yu650715>
2018-08-27 10:02:57 +0800

    Ehcache

--- springboot-15-ehcache/src/main/java/tk/yudady/controller/MusicController.java
+++ springboot-15-ehcache/src/main/java/tk/yudady/controller/MusicController.java
@@ -7,6 +7,7 @@ import org.springframework.cache.CacheManager;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import tk.yudady.service.EhcacheService;
 import tk.yudady.service.MusicService;
 @RestController
 public class MusicController {
@@ -18,6 +19,11 @@ public class MusicController {
 
 	@Autowired
 	private CacheManager cacheManager;
+
+	@Autowired
+	private EhcacheService ehcacheService;
+
+
 
 	@GetMapping("/")
 	public Object index() throws Exception {
@@ -36,6 +42,82 @@ public class MusicController {
 		play("trombone");
 		play("trombone");
 		play("trombone");
+
+
+
+
+
+		System.out.println("第一次调用：" + ehcacheService.getTimestamp("param"));
+		Thread.sleep(2000);
+		System.out.println("2秒之后调用：" + ehcacheService.getTimestamp("param"));
+		Thread.sleep(4000);
+		System.out.println("再过4秒之后调用：" + ehcacheService.getTimestamp("param"));
+
+
+		String key = "mengdee";
+		ehcacheService.refreshData(key);  // 模拟从数据库中加载数据
+		String data = ehcacheService.getDataFromDB(key);
+		System.out.println("data:" + data); // data:mengdee::103385
+
+		ehcacheService.refreshData(key);  // 模拟从数据库中加载数据
+		String data2 = ehcacheService.getDataFromDB(key);
+		System.out.println("data2:" + data2);   // data2:mengdee::180538
+
+
+
+
+
+
+
+		ehcacheService.findById("1"); // 模拟从数据库中查询数据
+		ehcacheService.findById("1");
+
+
+
+
+
+
+
+
+		ehcacheService.isReserved("123");
+		ehcacheService.isReserved("123");
+
+
+
+
+
+
+
+
+		// 线添加到缓存
+		ehcacheService.findById("1");
+
+		// 再删除
+		ehcacheService.removeUser("1");
+
+		// 如果不存在会执行方法体
+		ehcacheService.findById("1");
+
+
+
+
+
+
+
+
+		ehcacheService.findById("1");
+		ehcacheService.findById("2");
+
+		ehcacheService.removeAllUser();
+
+		ehcacheService.findById("1");
+		ehcacheService.findById("2");
+
+		//      模拟从数据库中查询数据
+		//      模拟从数据库中查询数据
+		//      UserCache delete all
+		//      模拟从数据库中查询数据
+		//      模拟从数据库中查询数据
 
 		return "hello";
 	}
