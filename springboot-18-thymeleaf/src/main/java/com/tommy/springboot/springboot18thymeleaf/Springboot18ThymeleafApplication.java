f56b7115e109171c03f02ee623a03923833c3920
yu_dady@yahoo.com.tw <yu650715>
2018-12-09 21:45:12 +0800

    thymeleaf

---
+++ springboot-18-thymeleaf/src/main/java/com/tommy/springboot/springboot18thymeleaf/Springboot18ThymeleafApplication.java
@@ -0,0 +1,12 @@
+package com.tommy.springboot.springboot18thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot18ThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot18ThymeleafApplication.class, args);
+	}
+}
