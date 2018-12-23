1c085b8285d3137eccd6398b6fe28a0367c7381e
yu_dady@yahoo.com.tw <yu650715>
2018-12-23 23:26:27 +0800

    elastic search

--- springboot-22-elasticsearch/src/test/java/com/tommy/Test01ElasticSearch.java
+++ springboot-22-elasticsearch/src/test/java/com/tommy/Test01ElasticSearch.java
@@ -7,18 +7,26 @@ import java.util.List;
 import com.sun.org.apache.bcel.internal.generic.NEW;
 import com.tommy.model.EsMatchCompetitor;
 import com.tommy.model.EsMatchMarketArgument;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.search.aggregations.Aggregations;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
-import org.springframework.data.elasticsearch.core.query.IndexQuery;
-import org.springframework.data.elasticsearch.core.query.IndexQueryBuilder;
+import org.springframework.data.elasticsearch.core.aggregation.AggregatedPage;
+import org.springframework.data.elasticsearch.core.query.*;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.tommy.model.EsMatchInfoDoc;
+
+import static org.elasticsearch.index.query.QueryBuilders.boolQuery;
+import static org.elasticsearch.index.query.QueryBuilders.rangeQuery;
+import static org.elasticsearch.index.query.QueryBuilders.termQuery;
 
 @RunWith(SpringJUnit4ClassRunner.class) //
 @SpringBootTest(classes = App.class)
@@ -50,7 +58,23 @@ public class Test01ElasticSearch {
 
 	}
 
+    @Test
+    public void test01Read() throws Exception {
+        BoolQueryBuilder boolQueryBuilder = boolQuery().
+                must(rangeQuery("matchTime").gte("20180101010101").lt("20580101010101").format("yyyyMMddHHmmss"))
+                ;
 
+        SearchQuery searchQuery = new NativeSearchQueryBuilder()
+                .withQuery(boolQueryBuilder)
+                .withPageable(PageRequest.of(0, 10))
+                .build();
+
+        AggregatedPage<EsMatchInfoDoc> esMatchInfoDocs = elasticsearchTemplate.queryForPage(searchQuery, EsMatchInfoDoc.class);
+        System.out.println(esMatchInfoDocs.getContent().size());
+        System.out.println(esMatchInfoDocs.getContent().size());
+        System.out.println(esMatchInfoDocs.getContent().size());
+        esMatchInfoDocs.stream().forEach(System.out::println);
+    }
 
 
 
---
+++ springboot-22-elasticsearch/src/test/java/com/tommy/Test02SearchResponse.java
@@ -0,0 +1,113 @@
+package com.tommy;
+
+import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
+
+import java.util.Date;
+import java.util.Iterator;
+import java.util.concurrent.TimeUnit;
+
+import org.elasticsearch.action.ActionFuture;
+import org.elasticsearch.action.ActionListener;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class) //
+@SpringBootTest(classes = App.class)
+public class Test02SearchResponse {
+
+	@Autowired
+	ElasticsearchTemplate elasticsearchTemplate;
+
+	@Test
+	public void index() throws Exception {
+		Client client = elasticsearchTemplate.getClient();
+
+		String id = "" + new Date().getTime();
+		XContentBuilder builder = jsonBuilder()//
+			.startObject()//
+			.field("id", id)//
+			.field("user", "kimchy")//
+			.field("postDate", new Date())//
+			.field("age", 16)//
+			.field("message", "trying out Elasticsearch")//
+			.endObject();
+
+		IndexResponse response = client.prepareIndex("twitter1", "doc", id).setSource(builder).get();
+		System.out.println(response.getIndex());
+		System.out.println(response.toString());
+	}
+
+	@Test
+	public void search() throws Exception {
+		Client client = elasticsearchTemplate.getClient();
+		SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+		sourceBuilder.query(QueryBuilders.termQuery("user", "kimchy"));
+		sourceBuilder.from(0);
+		sourceBuilder.size(5);
+
+		SearchRequest searchRequest = new SearchRequest();
+		searchRequest.indices("twitter1");
+		searchRequest.source(sourceBuilder);
+
+		ActionFuture<SearchResponse> search = client.search(searchRequest);
+		SearchResponse searchResponse = search.actionGet();
+		System.out.println("******************");
+		System.out.println(searchResponse);
+		System.out.println(searchResponse.status());
+		Iterator<SearchHit> iterator = searchResponse.getHits().iterator();
+		while (iterator.hasNext()) {
+			SearchHit searchHit = iterator.next();
+			System.out.println(searchHit.getId() + "   " + searchHit.getSourceAsString());
+		}
+		System.out.println("******************");
+	}
+
+	@Test
+	public void searchAsync() throws Exception {
+		Client client = elasticsearchTemplate.getClient();
+		SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+		sourceBuilder.query(QueryBuilders.termQuery("user", "kimchy"));
+		sourceBuilder.from(0);
+		sourceBuilder.size(5);
+
+		SearchRequest searchRequest = new SearchRequest();
+		searchRequest.indices("twitter1");
+		searchRequest.source(sourceBuilder);
+
+		ActionListener<SearchResponse> listener = new ActionListener<SearchResponse>() {
+
+			@Override
+			public void onResponse(SearchResponse searchResponse) {
+				System.out.println("******************");
+				System.out.println(searchResponse);
+				System.out.println(searchResponse.status());
+				Iterator<SearchHit> iterator = searchResponse.getHits().iterator();
+				while (iterator.hasNext()) {
+					SearchHit searchHit = iterator.next();
+					System.out.println(searchHit.getId() + " " + searchHit.getSourceAsString());
+				}
+				System.out.println("******************");
+			}
+
+			@Override
+			public void onFailure(Exception e) {
+				e.printStackTrace();
+			}
+		};
+		client.search(searchRequest, listener);
+
+		TimeUnit.SECONDS.sleep(5);
+	}
+}
